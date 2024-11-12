cfde39e89022b82521009b954fba096ca801bc9b
Mickaël Pham <mcpham@mural.co>
2024-11-12 10:39:09 +0100

    Basic authentication

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import expressBasicAuth from 'express-basic-auth'
 import { pinoHttp } from 'pino-http'
 import { logger } from './logger.js'
 
 export const app = express()
 
 app.use(pinoHttp({ logger }))
+
+app.use(expressBasicAuth({
+  users: {
+    mcpham: 'verysecret',
+    athomas: 'normal',
+  },
+  challenge: true,
+},
+))
+
 app.use(express.static('public'))
