dde3f9dc2b24d12d8be2a10a38e8dcff260393c2
Mickaël Pham <mcpham@mural.co>
2024-11-12 11:43:21 +0100

    Cookies are working

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cookieSession from 'cookie-session'
 import express from 'express'
-import expressBasicAuth from 'express-basic-auth'
 import { pinoHttp } from 'pino-http'
 import { logger } from './logger.js'
 
 export const app = express()
 
+app.set('views', './src/views')
+app.set('view engine', 'pug')
+
+app.use(bodyParser.urlencoded())
+app.use(cookieParser())
+app.use(cookieSession({ name: 'session', keys: ['some-server-side-secret'] }))
 app.use(pinoHttp({ logger }))
-
-app.use(expressBasicAuth({
-  users: {
-    mcpham: 'verysecret',
-    athomas: 'normal',
-  },
-  challenge: true,
-},
-))
-
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    headers: req.headers,
+    cookies: req.cookies,
+    session: req.session,
+  })
+})
+
+app.post('/name', (req, res) => {
+  const { name } = req.body
+  logger.info({ userName: name }, 'name received from request')
+  req.session.name = name
+  res.redirect('/')
+})
